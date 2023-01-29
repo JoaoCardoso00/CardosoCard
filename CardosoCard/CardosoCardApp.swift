a6acc8817148ef8e42926cfbf011e1d9b0f48ea0
JoaoCardoso00 <jvcq13@gmail.com>
2023-01-29 17:22:11 -0300

    Initial Commit

---
+++ CardosoCard/CardosoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardosoCardApp.swift
+//  CardosoCard
+//
+//  Created by joao cardoso on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CardosoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
